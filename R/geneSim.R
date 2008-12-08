714adc6875914b83b673bcf93f6eac976676388d
m.carlson <m.carlson@bc3139a8-67e5-0310-9ffc-ced21a209358>
2008-12-08 18:18:48 +0000

    Adds GOSemSim to the repository.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@35719 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/geneSim.R
@@ -0,0 +1,12 @@
+`geneSim` <-
+function(gene1, gene2, ont="MF", drop="IEA", weight.isa = 0.8, weight.partof = 0.6){
+	wh_ont <- match.arg(ont, c("MF", "BP", "CC"))
+	go1 <- ygcGetOnt(gene1, ontology= ont, dropCodes=drop)
+	go2 <- ygcGetOnt(gene2, ontology= ont, dropCodes=drop)
+	if (length(go1) == 0 || length(go2) == 0) {
+		return (list(geneSim=NA, GO1=go1, GO2=go2))
+	}
+	geneSim <- mgoSim(go1,go2, wh_ont, weight.isa, weight.partof)
+	geneSim <- round(geneSim, digits=3)
+	return (list(geneSim=geneSim, GO1=go1, GO2=go2))
+}
