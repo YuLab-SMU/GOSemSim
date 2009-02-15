f92e4f935307a5580b5ddc14f1626f2612c22c76
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-02-15 06:09:41 +0000

    add four other methods:Resnik, Rel, Jiang, Lin.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@37414 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GOSemSim-internal.R
+++ R/GOSemSim-internal.R
@@ -28,7 +28,7 @@ function(query.go, weight.isa = 0.8, weight.partof = 0.6)
 	sw <- 1
 	n <- length(query.go)
 	if (n > 3) {
-		sequence <- seq(1, n-3, by=2)	##drop the 'all' node
+		sequence <- seq(1, n-1, by=2)	
 		w <- 1
 		for(i in sequence) {
 			old.w <- w
@@ -61,6 +61,9 @@ function(query.go, weight.isa = 0.8, weight.partof = 0.6)
 `ygcGetOnt` <-
   function(gene, ontology, dropCodes) {
     allGO <- org.Hs.egGO[[gene]]
+    if (is.null(allGO)) {
+    	return (NA)
+    }
     if (sum(!is.na(allGO)) == 0) {
     	return (NA)
     }
@@ -72,4 +75,74 @@ function(query.go, weight.isa = 0.8, weight.partof = 0.6)
     
     category<-sapply(allGO, function(x) x$Ontology)
     return (unlist(unique(names(allGO[category %in% ontology]))))
+}
+
+`WangMethod` <- 
+function(GOID1, GOID2, ont="MF"){
+	wh_ont <- match.arg(ont, c("MF", "BP", "CC"))
+	weight.isa = 0.8
+	weight.partof = 0.6
+	if (GOID1 == GOID2)
+		return (gosim=1)
+	if (!exists("GOSemSimenvironment")) 
+		.initial()
+	if (exists("Ancestor", envir=GOSemSimenvironment)) {
+		Ancestor <- get("Ancestor", envir=GOSemSimenvironment)	
+		go1 <- Ancestor[[which(names(Ancestor)==GOID1)]]
+		go2 <- Ancestor[[which(names(Ancestor)==GOID2)]]
+	} else {
+		go1 <- ygcQueryAncestor(GOID1, wh_ont)
+		go2 <- ygcQueryAncestor(GOID2, wh_ont)
+	}	
+	
+	size1 <- length(go1)
+	size2 <- length(go2)
+	if (size1 == 0 || size2 == 0)	return (NA)
+	
+	sv1 <- ygcSemVal(go1, weight.isa, weight.partof)
+	sv2 <- ygcSemVal(go2, weight.isa, weight.partof)
+	
+	ancestor1 <- unlist(go1[seq(1, length(go1)-3, by=2)])
+	ancestor2 <- unlist(go2[seq(1, length(go2)-3, by=2)])
+	ancestor1 <- c(GOID1, ancestor1)
+	ancestor2 <- c(GOID2, ancestor2)
+	
+	commonAncestor<-intersect(ancestor1, ancestor2)
+	ca1.idx <- unlist(sapply(commonAncestor, function(x) which(ancestor1==x)))
+	ca2.idx <- unlist(sapply(commonAncestor, function(x) which(ancestor2==x)))
+	
+	gosim <- sum(sv1$sw[ca1.idx], sv2$sw[ca2.idx]) / (sv1$sv + sv2$sv)
+	gosim <- round(gosim, digits=3)
+	
+	return (gosim)
+}
+
+`InfoContentMethod` <- function(GOID1, GOID2, ont, measure) {
+	cnt <- "Count"
+	data("GOCount", package="GOSemSim")
+	rootCount <- switch(ont, 
+                     MF=GOCount["GO:0003674", cnt],
+                     BP=GOCount["GO:0008150", cnt],
+                     CC=GOCount["GO:0005575", cnt] )
+	p1 <- GOCount[GOID1, cnt]/rootCount
+	p2 <- GOCount[GOID2, cnt]/rootCount        
+	if (p1 == 0 || p2 == 0) return (NA)
+	
+	Ancestor <- switch(ont,
+		MF = AnnotationDbi::as.list(GOMFANCESTOR) ,
+		BP = AnnotationDbi::as.list(GOBPANCESTOR) , 
+		CC = AnnotationDbi::as.list(GOCCANCESTOR)	)
+		
+	ancestor1 <- unlist(Ancestor[GOID1])
+	ancestor2 <- unlist(Ancestor[GOID2])
+	commonAncestor <- intersect(ancestor1, ancestor2)
+	if (length(commonAncestor) == 0) return (NA)
+	pms <- min(GOCount[commonAncestor,cnt], na.rm=TRUE)/rootCount
+	
+	sim<-switch(measure,
+   	    Resnik=-log(pms),
+   	    Lin=2*log(pms)/(log(p1)+log(p2)),
+   	    Jiang=2*log(pms)-log(p1)-log(p2), 
+   	    Rel=(2*log(pms)/(log(p1)+log(p2)))*(1-pms) )	
+	return(round(sim, digits=3))
 }
--- R/mgeneSim.R
+++ R/mgeneSim.R
@@ -1,21 +1,26 @@
 `mgeneSim` <-
-function (genes, ont="MF", drop="IEA", weight.isa = 0.8, weight.partof = 0.6){
+function (genes, ont="MF", drop="IEA", measure="Resnik"){
 	wh_ont <- match.arg(ont, c("MF", "BP", "CC"))
+	wh_measure <- match.arg(measure, c("Resnik", "Jiang", "Lin", "Rel", "Wang"))
+	
 	genes <- genes[!is.na(genes)]
 	n <- length(genes)
 	gos <- sapply(genes, ontology=wh_ont, dropCodes=drop, ygcGetOnt)
 	gos <- gos[!is.na(gos)]
 	genes <- genes[!is.na(gos)]
-	if (!exists("GOSemSimenvironment")) 
-		.initial()
-	Ancestor <- assign("Ancestor", GOSemSimenvironment)
-	Ancestor <- ygcStoreAncestor(gos, wh_ont)
+	if (wh_measure == "Wang") {
+		if (!exists("GOSemSimenvironment")) 
+			.initial()
+		Ancestor <- assign("Ancestor", GOSemSimenvironment)
+		Ancestor <- ygcStoreAncestor(gos, wh_ont)
+	}
+	
 	simMatrix <- matrix(NA, nrow=n, ncol=n)
 	colnames(simMatrix) <- genes
 	rownames(simMatrix) <- genes
 	for (i in 1:n) {
 		for (j in 1:i) {
-			simMatrix[i,j] <- geneSim(genes[i], genes[j], ont=wh_ont, drop="IEA", weight.isa= weight.isa, weight.partof=weight.partof)$geneSim
+			simMatrix[i,j] <- geneSim(genes[i], genes[j], wh_ont, drop, wh_measure)$geneSim
 			if (i != j)	simMatrix[j,i] <- simMatrix[i,j]
 		}
 	}
