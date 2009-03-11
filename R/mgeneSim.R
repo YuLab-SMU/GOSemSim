94788d6eb139caee0fedcd62a941e1f252244dd7
Guangchuang Yu <guangchuangyu@gmail.com>
2009-03-11 09:35:36 +0000

    Add two function *clusterSim* and *mclusterSim*
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@37905 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/mgeneSim.R
+++ R/mgeneSim.R
@@ -1,4 +1,4 @@
-`mgeneSim` <- function (genes, ont="MF", drop="IEA", organism="human", measure="Wang"){
+`mgeneSim` <- function (genes, ont="MF", organism="human", measure="Wang", drop="IEA"){
 	wh_ont <- match.arg(ont, c("MF", "BP", "CC"))
 	wh_measure <- match.arg(measure, c("Resnik", "Jiang", "Lin", "Rel", "Wang"))
 	wh_organism <- match.arg(organism, c("human", "fly", "mouse", "rat", "yeast"))
@@ -14,10 +14,13 @@
 	rownames(simMatrix) <- genes
 	for (i in 1:n) {
 		for (j in 1:i) {
-			simMatrix[i,j] <- geneSim(genes[i], genes[j], wh_ont, drop, wh_measure)$geneSim
-			if (i != j)	simMatrix[j,i] <- simMatrix[i,j]
+			simMatrix[i,j] <- geneSim(genes[i], genes[j], wh_ont, wh_organism, wh_measure, drop)$geneSim
+			if (i != j)	{
+				simMatrix[j,i] <- simMatrix[i,j]
+			}
 		}
 	}
+	
 	removeNA <- apply(!is.na(simMatrix), 1, sum)>0
 
 	return(simMatrix[removeNA, removeNA])
