b4f72229746e38d9b63ef9eb53dbff99ff66d839
Guangchuang Yu <guangchuangyu@gmail.com>
2009-03-11 09:32:55 +0000

    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@37904 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/mclusterSim.R
@@ -0,0 +1,23 @@
+`mclusterSim` <- 
+function(clusters, ont="MF", organism="human", measure="Wang", drop="IEA") {
+	wh_ont <- match.arg(ont, c("MF", "BP", "CC"))
+	wh_measure <- match.arg(measure, c("Resnik", "Jiang", "Lin", "Rel", "Wang"))
+	wh_organism <- match.arg(organism, c("human", "fly", "mouse", "rat", "yeast"))
+	
+	size <- length(clusters)
+	simmat <- matrix(NA, nrow=size, ncol=size)
+	rownames(simmat) <- names(clusters)
+	colnames(simmat) <- names(clusters)
+	
+	for (i in 1:size) {
+		for (j in 1:i) {
+			simmat[i,j] <- clusterSim(clusters[[i]], clusters[[j]], wh_ont, wh_organism, wh_measure, drop)
+			if ( i != j) {
+				simmat[j, i] <- simmat[i,j]
+			} 
+		}
+	}	
+	
+	removeNA <- apply(!is.na(simmat), 1, sum) > 0
+	return(simmat[removeNA, removeNA])
+}
