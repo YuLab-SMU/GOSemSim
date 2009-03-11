6e8d10fd7bd3ef73403f5f827feed299e2355b71
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2009-03-11 09:32:23 +0000

    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@37903 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/clusterSim.R
@@ -0,0 +1,26 @@
+`clusterSim` <- 
+function(cluster1, cluster2, ont="MF", organism="human", measure="Wang", drop="IEA"){
+	wh_ont <- match.arg(ont, c("MF", "BP", "CC"))
+	wh_organism <- match.arg(organism, c("human", "fly", "mouse", "rat", "yeast"))
+	wh_measure <- match.arg(measure, c("Resnik", "Jiang", "Lin", "Rel", "Wang"))
+	
+	size1 <- length(cluster1)
+	size2 <- length(cluster2)
+	if (size1 == 0 || size2 == 0) {
+		return (NA)
+	}
+	
+	allSim <- matrix(data=NA, nrow=size1, ncol=size2)
+	for (i in 1:size1) {
+		for (j in 1:size2){
+			allSim[i,j] <- geneSim(cluster1[i], cluster2[j], wh_ont, wh_organism, wh_measure, drop)$geneSim
+		}
+	}	
+	
+	if (!sum(!is.na(allSim))) {
+		return (NA)
+	}
+	
+	result <- max(allSim, na.rm=TRUE)
+	return (round(result, digits=3))
+}
