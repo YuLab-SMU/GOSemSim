3ce4a2e5ea7c186bd2a0d4880849c6ba3df0f35e
Guangchuang Yu <guangchuangyu@gmail.com>
2012-06-14 11:38:43 +0000

    export termSim, which can be used in    other ontological semantic similarity measurement <2012-06-14, Thu>
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@66725 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ClassGOSet.R
+++ R/ClassGOSet.R
@@ -4,39 +4,16 @@ setMethod(
           f          = "sim",
           signature  = "GOSet",
           definition =function(object, params){
-		ICmethods <- c("Resnik", "Jiang", "Lin", "Rel")
+
                 go1  <- object@GOSet1
                 go2  <- object@GOSet2
-                go1  <- unique(go1)
-                go2  <- unique(go2)
 
-		m                <- length(go1)
-		n                <- length(go2)
-		scores           <- matrix(nrow=m, ncol=n)
-		rownames(scores) <- go1
-		colnames(scores) <- go2
+                ont <- params@ontology
+                method <- params@method
+                organism <- params@organism
 
-		ic = params@method %in% ICmethods
-		for( i in 1:m) {
-                    for (j in 1:n) {
-                        if ( is.na(go1[i]) || is.na(go2[j]) ) {
-                            scores[i,j] <- NA
-                        } else {
-                            if (ic) {
-                                scores[i,j] <- infoContentMethod(go1[i],
-                                                                 go2[j],
-                                                                 ont      =params@ontology,
-                                                                 method   =params@method,
-                                                                 organism =params@organism)
-                            }
-                            if (params@method == "Wang") {
-                                scores[i,j] <- wangMethod(go1[i],
-                                                          go2[j],
-                                                          ont=params@ontology)
-                            }
-                        }
-                    }
-		}
+                scores <- termSim(go1,go2, method, organism, ont)
+
 		result <- combineScores(scores, params@combine)
 		return(result)
             }
