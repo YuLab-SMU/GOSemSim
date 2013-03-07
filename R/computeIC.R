25a6c51a81d62f994fb575f98576c92e54f1b440
Guangchuang Yu <guangchuangyu@gmail.com>
2013-03-07 05:07:07 +0000

    skip if already installed in install_dependent_db function
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@73998 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/computeIC.R
+++ R/computeIC.R
@@ -97,11 +97,14 @@ rebuildAllICdata <- function() {
 
 
 install_dependent_db <- function() {
+    require("BiocInstaller", character.only=TRUE)
+    biocLite <- eval(parse(text="biocLite"))
+
     species <- get("SupportedSpecies",envir=GOSemSimEnv)
     for (organism in species) {
         annoDb <- getDb(organism)
-        require("BiocInstaller", character.only=TRUE)
-        biocLite <- eval(parse(text="biocLite"))
-        biocLite(annoDb)
+        if (!require(annoDb, character.only=TRUE)) {
+            biocLite(annoDb)
+        }
     }
 }
