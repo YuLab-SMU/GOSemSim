7396b5324a8bb22b09fa0196198a684b1d5f849a
Guangchuang Yu <guangchuangyu@gmail.com>
2014-06-01 00:32:40 +0800

    move to github

---
+++ R/utilities.R
@@ -0,0 +1,58 @@
+.initial <- function() {
+    assign("GOSemSimEnv", new.env(),.GlobalEnv)
+    assign("SemSimCache", new.env(), .GlobalEnv)
+    assign("ICEnv", new.env(), .GlobalEnv)
+    assign("SupportedSpecies", c("anopheles",
+                                 "arabidopsis",
+                                 "bovine",
+                                 "canine",
+                                 "chicken",
+                                 "chimp",
+                                 "ecolik12",
+                                 "ecsakai",
+                                 "fly",
+                                 "human",
+                                 "malaria",
+                                 "mouse",
+                                 "pig",
+                                 "rat",
+                                 "rhesus",
+                                 "worm",
+                                 "xenopus",
+                                 "yeast",
+                                 "zebrafish"),
+           envir=GOSemSimEnv)
+    ## remove "coelicolor" as it is not supported by Bioconductor
+}
+
+##' get supported organisms
+##'
+##'
+##' @title getSupported_Org
+##' @return supported organisms
+##' @export
+##' @author Yu Guangchuang
+getSupported_Org <- function() {
+    if (!exists("GOSemSimEnv")) .initial()
+    supported_Org <- get("SupportedSpecies", envir=GOSemSimEnv)
+    return(supported_Org)
+}
+
+##' @importFrom GO.db GOMFPARENTS
+##' @importFrom GO.db GOBPPARENTS
+##' @importFrom GO.db GOCCPARENTS
+.getParents <- function(ont) {
+    Parents <- switch(ont,
+                      MF = "GOMFPARENTS",
+                      BP = "GOBPPARENTS",
+                      CC = "GOCCPARENTS",
+                      DO = "DOPARENTS"
+                      )
+    if (ont == "DO") {
+        db <- "DO.db"
+        require(db, character.only=TRUE)
+    }
+    Parents <- eval(parse(text=Parents))
+    return(Parents)
+}
+
