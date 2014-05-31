745d2c4c54e12ae31efce9bdc2e53842c75461df
g.yu <g.yu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2014-05-31 16:29:30 +0000

    setting up git-svn bridge
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@90899 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++
@@ -1,6 +0,0 @@
-.onLoad <- function(libname, pkgname) {
-                                        #	pkgVersion <- packageDescription(pkgname, fields="Version")
-                                        #	msg <- paste("\nWelcome to", pkgname, "version", pkgVersion, "\n")
-                                        #	packageStartupMessage(msg)
-    .initial()
-}
