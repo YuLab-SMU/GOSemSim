0649505679aae342b9a500678058882f96e7b29a
Guangchuang Yu <guangchuangyu@gmail.com>
2016-07-05 10:17:17 +0000

    version 1.99.0
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/GOSemSim@119142 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/godata.R
@@ -0,0 +1,76 @@
+##' prepare GO DATA for measuring semantic similarity
+##'
+##' 
+##' @title godata
+##' @param OrgDb OrgDb object
+##' @param keytype keytype
+##' @param ont one of 'BP', 'MF', 'CC'
+##' @param computeIC logical, whether computer IC
+##' @return GOSemSimDATA object
+##' @importFrom AnnotationDbi keys
+##' @importFrom AnnotationDbi select
+##' @importFrom AnnotationDbi metadata
+##' @export
+##' @author Guangchuang Yu
+godata <- function(OrgDb=NULL, keytype = "ENTREZID", ont, computeIC = TRUE) {
+    ont <- toupper(ont)
+    ont <- match.arg(ont, c("BP", "CC", "MF"))
+
+    if (is.null(OrgDb)) {
+        return(new("GOSemSimDATA",
+                   ont = ont))
+    }
+    
+    OrgDb <- load_OrgDb(OrgDb)
+    kk <- keys(OrgDb, keytype=keytype)
+    print('preparing gene to GO mapping data...')
+    goAnno <- suppressMessages(
+        select(OrgDb, keys=kk, keytype=keytype,
+               columns=c("GO", "ONTOLOGY")))
+    goAnno <- goAnno[goAnno$ONTOLOGY == ont,]
+    if (computeIC) {
+        print('preparing IC data...')
+        IC <- computeIC(goAnno, ont)
+    } else {
+        IC <- NULL
+    }
+    new("GOSemSimDATA",
+        keys = kk,
+        ont = ont,
+        IC = IC,
+        goAnno = goAnno,
+        metadata = metadata(OrgDb))
+}
+
+##' Class "GOSemSimDATA"
+##' This class stores IC and gene to go mapping for semantic similarity measurement
+##'
+##'
+##' @name GOSemSimDATA-class
+##' @aliases GOSemSimDATA-class
+##'   show,GOSemSimDATA-method
+##' 
+##' @docType class
+##' @slot keys gene ID
+##' @slot ont ontology
+##' @slot IC IC data
+##' @slot goAnno gene to GO mapping
+##' @slot metadata metadata
+##' @exportClass GOSemSimDATA
+##' @keywords classes
+##' @importFrom methods setClass
+setClass("GOSemSimDATA",
+         representation = representation(
+             keys = "character",
+             ont = "character",
+             IC = "numeric",
+             goAnno = "data.frame",
+             metadata = "data.frame"
+         ))
+
+##' @importFrom methods setMethod
+setMethod("show", signature(object = "GOSemSimDATA"),
+          function(object) {
+              cat("#\n# DATA for GOSemSim ...\n#\n")
+          })
+             
