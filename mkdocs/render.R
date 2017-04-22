04106b5af317b801ecf19766fa2a83b7ed306c52
guangchuang yu <guangchuangyu@gamil.com>
2017-04-22 13:39:34 +0800

    update render

--- mkdocs/render.R
+++ mkdocs/render.R
@@ -8,6 +8,7 @@ outfiles <- paste0("../docs/", files)
 outfiles <- sub("rmd$", "md", outfiles)
 
 for (i in seq_along(files)) {
+    message("processing ", files[i])
     render(files[i], output_file=outfiles[i])
 }
 
