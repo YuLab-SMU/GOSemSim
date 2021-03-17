d3dfe322e777a7c1ac61c60eced847eedb465ae6
qibaiqi <1178431277@qq.com>
2021-03-17 23:41:28 +0800

    To tradeoff the runtime and accuracy, all possible cutoffs is selected in two steps, roughly compute first and then refine the result in smaller gap.

--- R/TCSScutoff.R
+++ R/TCSScutoff.R
@@ -31,7 +31,7 @@
 #'     len <- nrow(ppi)
 #'
 #'     #select length
-#'     s_len <- 500
+#'     s_len <- 100
 #'     pos_1 <- sample(len, s_len, replace = T)
 #'     #negative set
 #'     pos_2 <- sample(len, s_len, replace = T)
@@ -44,15 +44,15 @@
 #'      stringsAsFactors = FALSE)
 #'
 #'     cutoff <- tcss_cutoff(OrgDb = org.Hs.eg.db, keytype = "ENSEMBLPROT",
-#'     ont = "BP", combine_method = "max", ppidata)
+#'     ont = "MF", combine_method = "max", ppidata)
 #' }
 tcss_cutoff <- function(OrgDb = NULL, keytype = "ENTREZID", ont,
-                       combine_method = "max", ppidata) {
-
+                        combine_method = "max", ppidata) {
+    #compute IC value
     semdata <- godata(OrgDb, keytype = keytype, ont = ont, computeIC = TRUE,
-                        processTCSS = FALSE, cutoff = NULL)
-    #cutoff is in the range of ICT value
+                      processTCSS = FALSE, cutoff = NULL)
     IC <- semdata@IC
+    #GO means all annotated terms in the species
     GO <- names(IC[!is.infinite(IC)])
     offspring <- switch(ont,
                         MF = AnnotationDbi::as.list(GOMFOFFSPRING),
@@ -60,23 +60,33 @@ tcss_cutoff <- function(OrgDb = NULL, keytype = "ENTREZID", ont,
                         CC = AnnotationDbi::as.list(GOCCOFFSPRING))
     #compute ICT value for each term
     ICT <- computeICT(GO, offspring)
-    #cutoffs, all possible cutoff values
-    cutoffs <- seq(0.1, max(ICT) + 0.1, by = 0.1)
     #all genes/proteins that have none-zero annotations
     all_pro <- unique(semdata@geneAnno[, keytype])
     #filter the ppidata
     filtered_ppidata <- create_filtered_ppidata(all_pro, ppidata = ppidata)
-    #calcualte the similarity value for filtered_ppidata
-    predict_result <- lapply(cutoffs, computePre,
-                             filtered_ppidata = filtered_ppidata,
-                             semdata = semdata,
-                             combine_method = combine_method)
-
-    #calculate the auc valur and F1_score
-    auc_F1_score <- calc_auc_F1_score(predict_result,
-                                      filtered_ppidata = filtered_ppidata)
-    #decide the most appropriate cutoff
-    decide_cutoff(auc_F1_score, cutoffs = cutoffs)
+    #all possible cutoffs in big gap
+    initial_cutoffs <- seq(0.1, max(ICT) + 0.5, by = 0.5)
+    #compute prediction value on filtered_ppidata
+    initial_predict <- lapply(initial_cutoffs, computePre,
+                              filtered_ppidata = filtered_ppidata,
+                              semdata = semdata,
+                              combine_method = combine_method)
+    #select best cutoff considering auc and F1-score value
+    i_pos <- best_cutoff_position(initial_predict,
+                                  filtered_ppidata = filtered_ppidata)
+    #possible cutoffs in small gap
+    final_cutoffs <- seq(initial_cutoffs[min(i_pos)],
+                         initial_cutoffs[max(i_pos)], by = 0.1)
+    #compute prediction value again
+    final_predict <- lapply(final_cutoffs, computePre,
+                            filtered_ppidata = filtered_ppidata,
+                            semdata = semdata,
+                            combine_method = combine_method)
+    #select best cutoff again
+    f_pos <- best_cutoff_position(final_predict,
+                                  filtered_ppidata = filtered_ppidata)
+    #for all possible best-cutoffs, return the smallest one for time saving
+    final_cutoffs[min(f_pos)]
 }
 
 #' keep the proteins with none-zero annotations
@@ -90,34 +100,34 @@ tcss_cutoff <- function(OrgDb = NULL, keytype = "ENTREZID", ont,
 create_filtered_ppidata <- function(all_pro, ppidata) {
     #check data type
     if (!(is.character(ppidata[, 1]) && is.character(ppidata[, 2]) &&
-        is.logical(ppidata[, 3]))) {
+          is.logical(ppidata[, 3]))) {
         stop("ppidata must be a data.frame with three columns:character, character, logical")
     }
-
+    
     ppidata <- na.omit(ppidata)
-
+    
     #remove those proteins that have zero annotations
     len1 <- ppidata[, 1] %in% all_pro
     len2 <- ppidata[, 2] %in% all_pro
     ppidata_exist <- ppidata[len1 & len2, ]
     filtered_ppidata <- unique(ppidata_exist)
-
+    
     len <- dim(filtered_ppidata)[1]
-
+    
     if (len == 0) {
         stop("filtered ppidata is empty, none items have GO annotation. Please input more data.")
     }
-
+    
     nTrue <- sum(filtered_ppidata[, 3])
     nFalse <- len - nTrue
-
+    
     if (nTrue == len || nFalse == len) {
         stop("The filtered ppidata lacks the necessary label:TRUE and FALSE. Please input more data.")
     }
-
+    
     message(paste("positive set has", nTrue,
                   "PPI pairs, negative set has", nFalse, "PPI pairs"))
-
+    
     return(filtered_ppidata)
 }
 
@@ -130,78 +140,48 @@ create_filtered_ppidata <- function(all_pro, ppidata) {
 #' @return list, the prediction value for the cutoff
 #' @noRd
 #'
-computePre <- function(cutoff, filtered_ppidata, semdata,
-                       combine_method) {
+computePre <- function(cutoff, filtered_ppidata, semdata, combine_method) {
     #tcssdata is updated with this input cutoff
     tcssdata <- process_tcss(semdata@ont, semdata@IC, cutoff = cutoff)
     semdata@tcssdata <- tcssdata
+    #NA is produced when cutoff out of range
     semdata@tcssdata <- na.omit(tcssdata)
     #similarity value is calculated with the semdata
     mapply(function(e, f) geneSim(e, f,
                                   semData = semdata,
                                   measure = "TCSS",
                                   combine = combine_method),
-                             filtered_ppidata[, 1], filtered_ppidata[, 2])
+           filtered_ppidata[, 1], filtered_ppidata[, 2])
 }
 
-#' calculate auc and F1-score
+#' the best cutoffs' position considering auc anf F1-score value
 #'
 #' @param predict_result list, prediction value for all cutoffs
 #' @param filtered_ppidata data.frame, annotated protein pairs and their labels
-#' @return data.frame, auc and F1-score value for different cutoffs
+#' @return vector, the position number in cutoffs
 #' @noRd
-#'
-calc_auc_F1_score <- function(predict_result, filtered_ppidata) {
+best_cutoff_position <- function(predict_result, filtered_ppidata) {
     #geneSim returns one value and two characters in once calculation
     len <- dim(filtered_ppidata)[1]
     value_loc <- seq(from = 1, to = len * 3, by = 3)
     #just the similarity value
-    pre_value <- lapply(predict_result, function(p) as.numeric(p[value_loc]))
-
+    pre_value <- lapply(predict_result, function(e) as.numeric(e[value_loc]))
+    
     #auc value
-    auc <- vapply(pre_value, function(pv)
-        ROCR::performance(ROCR::prediction(pv, filtered_ppidata[, 3],
+    auc <- vapply(pre_value, function(e)
+        ROCR::performance(ROCR::prediction(e, filtered_ppidata[, 3],
                                            label.ordering = c(FALSE, TRUE)),
                           measure = "auc")@y.values[[1]], numeric(1))
-
+    
     #F1_score at different semantic similarity cutoffs
-    all_F1_score <- lapply(pre_value, function(pv)
-        ROCR::performance(ROCR::prediction(pv, filtered_ppidata[, 3],
+    all_F1_score <- lapply(pre_value, function(e)
+        ROCR::performance(ROCR::prediction(e, filtered_ppidata[, 3],
                                            label.ordering = c(FALSE, TRUE)),
-                    measure = "f")@y.values[[1]])
+                          measure = "f")@y.values[[1]])
     #average value
     F1_score <- vapply(all_F1_score, mean, na.rm = TRUE, numeric(1))
-
-    #return as data.frame
-    return(data.frame(auc = auc,
-                      F1_score = F1_score,
-                      stringsAsFactors = F))
-}
-
-#' select the most appropriate cutoff
-#'
-#' @param auc_F1_score data.frame, auc and F1-score value for different cutoffs
-#' @param cutoffs vector, all possible cutoff values
-#' @return vector, topological cutoff for given parameters
-#' @noRd
-#'
-decide_cutoff <- function(auc_F1_score, cutoffs) {
-    #product value satisfies the "both maximized" requirement
-    auc_mutiply_F1 <- auc_F1_score[, "auc"] * auc_F1_score[, "F1_score"]
-    #get the max product value
-    pos <- which(auc_mutiply_F1 == max(auc_mutiply_F1))
-
-    if (length(pos) == 1)  return(cutoffs[pos])
-
-    #if not only one pair of auc and F1-score have same product
-    #take the one with larger auc
-    select_auc <- auc_F1_score[pos, "auc"]
-
-    auc_pos <- which(select_auc == max(select_auc))
-
-    if (length(auc_pos) == 1) return(cutoffs[pos[auc_pos]])
-
-    #if more than one pair of auc and F1-score are both same
-    #take the smaller cutoff for time saving
-    return(cutoffs[pos[min(auc_pos)]])
+    #multiplier effect helps to maximize both the auc and F1-score
+    auc_mutiply_F1 <- auc * F1_score
+    #get the position
+    which(auc_mutiply_F1 == max(auc_mutiply_F1))
 }
