a1387e359dae49ee938711a3f018be63c9c8d748
Guangchuang Yu <guangchuangyu@gmail.com>
2023-07-10 23:18:36 +0800

    Update readBlast2go.R
    rename parameter

--- R/readBlast2go.R
+++ R/readBlast2go.R
@@ -1,11 +1,11 @@
 ##'
 ##' given a BLAST2GO file, this function extracts the information from it and make it use for TERM2GENE.
 ##' @title read.blast2go
-##' @param Blast2GOFile BLAST2GO file
+##' @param file BLAST2GO file
 ##' @return a data frame with two columns: GO and Gene
 ##' @export
-read.blast2go <- function(Blast2GOFile) {
-    blast2go <- read.table(Blast2GOFile, header = TRUE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
+read.blast2go <- function(file) {
+    blast2go <- read.table(file, header = TRUE, sep = "\t", stringsAsFactors = FALSE, fill = TRUE)
     go_annotation_data <- blast2go[, c("Blast.Top.Hit.GOs", "Sequence.Name")]
     go_annotation_data <- tidyr::separate_rows(go_annotation_data, `Blast.Top.Hit.GOs`, sep = ", ")
 
